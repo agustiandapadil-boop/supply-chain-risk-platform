0be8162c9ea877cba1585bfe21747af54e8b5850
agustiandapadil-boop <agustiandapadil@gmail.com>
2026-07-28 09:24:33 +0700

    Update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,20 +3,17 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use Illuminate\Support\Facades\Hash; 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
-
     public function run(): void
     {
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::create([
+            'name' => 'Admin Utama',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('password'), 
+            'role' => 'admin', 
         ]);
     }
 }
